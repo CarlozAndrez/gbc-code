bc19f55afb65651ccfa381a0a288e174310ed912
mdkrajnak <mdkrajnak@gmail.com>
2011-01-31 03:21:52 +0000

    Add linearPath() and checkForObstructions() utility methods.

--- icypc1/src/Game.java
+++ icypc1/src/Game.java
@@ -12,7 +12,7 @@ class Game
 
 	/** Constants for the objects in each cell of the field */
 	public static final int GROUND_EMPTY = 0; // Just powdered snow in this
-											  // space.
+	                                          // space.
 	public static final int GROUND_TREE = 1; // A tree in this space
 	public static final int GROUND_S = 2; // A small snowball in this space
 	public static final int GROUND_M = 3; // A medium snowball in this space
@@ -22,20 +22,22 @@ class Game
 	public static final int GROUND_LS = 7; // A small snowball on a large one.
 	public static final int GROUND_SMR = 8; // A red Snowman in this space
 	public static final int GROUND_SMB = 9; // A blue Snowman in this space
+	public static final int GROUND_FRIEND = 10;
+	public static final int GROUND_FOE = 11;
 
 	/** Constants for the things a child can be holding */
 	public static final int HOLD_EMPTY = 0; // Child is holding nothing
 	public static final int HOLD_P1 = 1; // Child is holding one unit of
-										 // powdered snow
+	                                     // powdered snow
 	public static final int HOLD_P2 = 2; // Child is holding two units of
-										 // powdered snow
+	                                     // powdered snow
 	public static final int HOLD_P3 = 3; // Child is holding three units of
-										 // powdered snow
+	                                     // powdered snow
 	public static final int HOLD_S1 = 4; // Child is holding one small snowball.
 	public static final int HOLD_S2 = 5; // Child is holding two small
-										 // snowballs.
+	                                     // snowballs.
 	public static final int HOLD_S3 = 6; // Child is holding three small
-										 // snowballs.
+	                                     // snowballs.
 	public static final int HOLD_M = 7; // Child is holding one medium snowball.
 	public static final int HOLD_L = 8; // Child is holding one large snowball.
 
@@ -59,9 +61,12 @@ class Game
 
 	/** Snow that's too deep to move through. */
 	public static final int OBSTACLE_HEIGHT = 6;
-	
+
 	// Constant used to mark child locations in the map.
 	public static final int GROUND_CHILD = 10;
+
+	// Constant returned from checkForObstructions indicating one was not found.
+	private static final Point NO_OBSTRUCTION = new Point(-1, -1);
 
 	// Current game score for self (red) and opponent (blue).
 	int[] score = new int[2];
@@ -76,15 +81,15 @@ class Game
 	Child[] cList = new Child[2 * Game.CCOUNT];
 
 	int turnNum;
-	
+
 	// Set to null to turn of debug output
-	private static PrintStream dbgout = null; //System.err;
-	
+	private static PrintStream dbgout = null; // System.err;
+
 	public static void debug(String message)
 	{
 		if (dbgout != null) dbgout.println(message);
 	}
-	
+
 	public Game()
 	{
 		for (int i = 0; i < cList.length; i++)
@@ -96,7 +101,7 @@ class Game
 	{
 		turnNum = readTurn(in);
 		if (turnNum < 0) return false;
-		
+
 		Game.debug("turn: " + turnNum);
 
 		readField(in);
@@ -105,7 +110,7 @@ class Game
 
 		return true;
 	}
-	
+
 	private int readTurn(Scanner in)
 	{
 		String token = in.next();
@@ -149,8 +154,7 @@ class Game
 			readChild(in, cList[nchild]);
 
 			// Compute child color based on it's index.
-			cList[nchild].color = (nchild < Game.CCOUNT ? Game.RED
-			        : Game.BLUE);
+			cList[nchild].color = (nchild < Game.CCOUNT ? Game.RED : Game.BLUE);
 		}
 	}
 
@@ -172,7 +176,7 @@ class Game
 			// Record the child's location.
 			child.pos.x = Integer.parseInt(token);
 			child.pos.y = in.nextInt();
-			
+
 			Game.debug("token: " + token + ", pos: " + child.pos);
 
 			// Read the stance, what the child is holding and how much
@@ -185,7 +189,7 @@ class Game
 
 			child.dazed = in.nextInt();
 		}
-		
+
 		Game.debug("child: " + child);
 	}
 
@@ -202,10 +206,10 @@ class Game
 			}
 		}
 	}
-	
+
+	// Find the nearest neighbor (child or red snowman).
 	public int findNearestNeighbor(Point pos)
 	{
-		// Find the nearest neighbor.
 		int nearDist = 1000;
 		for (int i = 0; i < Game.SIZE; i++)
 			for (int j = 0; j < Game.SIZE; j++)
@@ -219,7 +223,78 @@ class Game
 					if (dx * dx + dy * dy < nearDist)
 					    nearDist = dx * dx + dy * dy;
 				}
-		
+
 		return nearDist;
+	}
+
+	public static int roundPathValue(double x)
+	{
+		if (x < 0)
+		{
+			return -(int) (Math.round(-x));
+		}
+
+		return (int) (Math.round(x));
+	}
+
+	// starts at integer position ( x1, y1 ) and moves toward position ( x2, y2 ) 
+	// The movement will be implemented as n steps, where n = max( | x2 -
+	// x1 |, | y2 - y1 | ). The steps will be spaced uniformly in time, with the
+	// first step occurring 1/n of the way through the turn and subsequent steps
+	// occurring at times 2/n, 3/n, … n/n during the turn. At time t/n, the
+	// entity moves to integer location ( x1 + round( ( t ( x2 - x1 ) )/n ), y1
+	// + round( ( t ( y2 - y1 ) )/n ) ). Thus, the entity will reach its
+	// destination exactly at the end of the turn. Rounding is performed using
+	// the following function. While the Java Math.round() functions rounds in
+	// the positive direction for values halfway between two integers, the
+	// function below rounds these values away from zero. This helps to ensure
+	// that linear paths are symmetric for the red and the blue player.
+	public Point[] linearPath(Point from, Point to, int steps)
+	{
+		int nsteps = Math.max(Math.abs(to.x - from.x), Math.abs(to.y - from.y));
+		Point[] results = new Point[nsteps];
+		for (int step = 0; step < nsteps; step++)
+		{
+			int time = step + 1;
+			double increment = time / nsteps;
+			results[step] = new Point(
+					from.x + roundPathValue(increment * (to.x - from.x)),
+					from.y + roundPathValue(increment * (to.y - from.y)));
+		}
+		return results;
+	}
+	
+	boolean hasFriend(Point pos)
+	{
+		for (int nchild = 0; nchild < Game.CCOUNT; nchild++)
+		{
+			if (cList[nchild].pos == pos) return true;
+		}
+		return false;
+	}
+	
+	boolean hasFoe(Point pos)
+	{
+		for (int nchild = Game.CCOUNT; nchild < (Game.CCOUNT * 2); nchild++)
+		{
+			if (cList[nchild].pos == pos) return true;
+		}
+		return false;
+	}
+	
+	public Point checkForObstructions(Point[] path)
+	{
+		for (int step = 0; step < path.length; step++)
+		{
+			if (ground[path[step].x][path[step].y] == GROUND_TREE) return path[step];
+			if (ground[path[step].x][path[step].y] == GROUND_SMR) return path[step];
+			if (ground[path[step].x][path[step].y] == GROUND_SMB) return path[step];
+			if (hasFriend(path[step])) return path[step];
+			if (hasFoe(path[step])) return path[step];
+			
+			// Return height as a negative value.
+			if (height[path[step].x][path[step].y] > 5) return path[step];
+		}
+		return NO_OBSTRUCTION;
 	}
 }
